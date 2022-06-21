4445fb0342457ab19041c100e990f09808b6affd
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-06-21 09:28:23 -0400

    Time: 39 ms (95.57%), Space: 34.8 MB (66.04%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -11,12 +11,13 @@
  */
 class Solution {
 public:
-    TreeNode* ans;
+   
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root==nullptr){return ans;}
-        if(root->val==val){ans=root;}
-        searchBST(root->left,val);
-        searchBST(root->right,val);
-        return ans;
+        while(root!=nullptr){
+            if(root->val==val){return root;}
+            if(root->val<val){root=root->right;}
+            else {root=root->left;}
+        }
+        return nullptr;
     }
 };
