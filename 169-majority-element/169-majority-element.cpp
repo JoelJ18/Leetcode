a5abb5cfe4165a7561c6a45c09a76616474bbee8
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-06-21 21:40:28 -0400

    Time: 23 ms (75.69%), Space: 19.6 MB (13.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        std::unordered_map<int,int>mp;
-        int max;
-        int ans;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>max){max=mp[nums[i]];ans=nums[i];}
-        }
-        return ans;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
