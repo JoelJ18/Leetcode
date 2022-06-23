8c7287656bdba9f940bf434762d097aa0284b3e7
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-06-23 10:53:52 -0400

    Time: 364 ms (12.88%), Space: 51.1 MB (92.65%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,39 +1,51 @@
 class MyHashMap {
 public:
-    vector<pair<int,int>> m;
+    struct ListNode {
+        int key;
+        int val;
+        ListNode* next;     
+        ListNode(int x) : key(x), val(0), next(NULL) {}
+        ListNode(int x, int y) : key(x), val(y), next(NULL) {}
+    };
+    
+    ListNode* head;
+    
     MyHashMap() {
-        
+        head = new ListNode(-1);  
     }
     
     void put(int key, int value) {
-        
-        int flag=-1;
-        for(int i=0;i<m.size();i++){
-            if(m[i].first==key){flag=i;break;}}
-        
-        if(flag==-1){m.push_back({key,value});}
-        else{m[flag].second=value;}
-        m.shrink_to_fit();
-        
-        
+        ListNode* temp = new ListNode(key, value);
+        ListNode* curr = head;
+        while(curr->next) {
+            curr = curr->next;
+            if(curr->key == key) {
+                curr->val = value;
+                return;
+            }
+        }
+        curr->next = temp;
     }
     
     int get(int key) {
-        for(auto i:m){if(i.first==key){return i.second;}}
+        ListNode* curr = head;
+        while(curr) {
+            if(curr->key == key)    return curr->val;
+            curr = curr->next;
+        }
         return -1;
     }
     
     void remove(int key) {
-        int count=0;
-        for(auto i:m){
-            if(i.first==key){
-                m.erase(m.begin()+count);
+        ListNode* curr = head;
+        while(curr) {
+            if(curr->next and curr->next->key == key) {
+                curr->next = curr->next->next;
             }
-            else{count++;}
+            curr = curr->next;
         }
     }
 };
-    
 
 
 /**
