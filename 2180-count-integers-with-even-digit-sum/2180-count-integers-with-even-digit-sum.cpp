76df0350ec375ff69c0094889ae7f95355d24bde
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-07-03 18:42:23 -0400

    Time: 0 ms (100.00%), Space: 5.8 MB (74.07%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -4,8 +4,9 @@ public:
         string s=to_string(num);
         int x=0;
         for(int i=0;i<s.length();i++){
-            x+=s[i];
+            x+=s[i]-48;
         }
+        cout<<x;
         if(num%2==0 && x%2==0){return num/2;}
         else if(num%2==0 && x%2!=0){return num/2 -1;}
         else {return num/2;}
