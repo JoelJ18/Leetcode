5db4045bc9c990f522955c3af2482408a0da6efc
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-07-27 10:27:13 -0400

    Time: 46 ms (24.85%), Space: 16.3 MB (69.77%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,26 +1,29 @@
 class MinStack {
 public:
-    vector<int> s;
-    
+    vector<pair<int,int>> s;
+    int min=INT_MAX;
     MinStack() {
         
     }
     
     void push(int val) {
-        s.push_back(val);
+        if(val<min){min=val;}
+        s.push_back({val,min});
         
     }
     
     void pop() {
         s.pop_back();
+        if(s.size()==0){min=INT_MAX;}
+        else{min=s[s.size()-1].second;}
     }
     
     int top() {
-        return s[s.size()-1];
+        return s[s.size()-1].first;
     }
     
     int getMin() {
-        return *min_element(s.begin(), s.end());
+        return s[s.size()-1].second;
     }
 };
 
