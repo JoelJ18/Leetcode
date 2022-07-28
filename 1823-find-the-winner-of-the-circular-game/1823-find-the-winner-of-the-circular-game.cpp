16bb28425dd881e0e06b9653671064d05c42ec64
JoelJ18 <101486509+JoelJ18@users.noreply.github.com>
2022-07-28 16:29:31 -0400

    Time: 113 ms (14.01%), Space: 24.6 MB (17.03%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        
+        queue<int> q;
+        int count=1;
+        for(int i=1;i<=n;i++){
+            q.push(i);
+        }
+        cout<<q.size();
+        
+        while(q.size()!=1){
+            
+            
+            if(count<k){
+                q.push(q.front());
+                q.pop();
+                
+            }
+            else{q.pop();count=0;}
+            
+            count++;
+        }
+        
+        return q.front();
+    }
+};
